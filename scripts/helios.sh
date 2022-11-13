65c0c1d469ffe592a3b3429809d81547c18bb065
Andreas Bigger <abigger87@gmail.com>
2022-11-13 14:34:31 -0500

    :gear: update helios script

--- scripts/helios.sh
+++ scripts/helios.sh
@@ -1,5 +1,7 @@
-/usr/bin/tmux new-session -d -s ENTER
-/usr/bin/tmux detach -s ENTER
-sleep 3
-/usr/bin/tmux send-keys -t 0 "helios" ENTER
+#! /bin/bash
+
+tmux kill-server  > /dev/null 2>&1
+tmux new-session -d -s "helios" &
+sleep 2
+tmux send-keys -t 0 "helios" ENTER
 echo "$(date) ${1} RESTARTED HELIOS"
