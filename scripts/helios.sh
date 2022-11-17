51557b17402bba10adcee6cbb26a95b03c82f39e
asnared <abigger87@gmail.com>
2022-11-17 08:02:14 -0500

    fixes

--- scripts/helios.sh
+++ scripts/helios.sh
@@ -3,5 +3,5 @@
 tmux kill-server  > /dev/null 2>&1
 tmux new-session -d -s "helios" &
 sleep 10
-tmux send-keys -t 0 "helios" ENTER
+tmux send-keys -t 0 "helios --execution-rpc \$ETH_RPC_URL" ENTER
 echo "$(date) ${1} RESTARTED HELIOS"
