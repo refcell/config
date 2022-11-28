9a57bf18d304148dea618546efc0d36ac2628d14
asnared <abigger87@gmail.com>
2022-11-27 20:55:59 -0800

    :building_construction: install scripts

--- scripts/installs.sh
+++ scripts/installs.sh
@@ -1,17 +1,26 @@
 #! /bin/bash
 
+# Curl Install
 sudo apt install curl
 
+# Rust
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 
+
+# Alacritty Terminal
+sudo apt-get install cmake pkg-config libfreetype6-dev libfontconfig1-dev libxcb-xfixes0-dev libxkbcommon-dev python3
+cargo install alacritty
+
+# nvim text editor
 sudo apt install neovim
 
+# Javascript Stuff
 sudo apt install nodejs
-
 sudo apt install npm
-
 sudo apt install yarn
 
+# Zsh shell
 sudo apt install zsh
+sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
 
