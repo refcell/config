82862cbda5d53f7b906b55275f45d978942c9be9
asnared <abigger87@gmail.com>
2022-11-28 11:28:20 -0800

    :whale: docker compose

--- scripts/installs.sh
+++ scripts/installs.sh
@@ -37,3 +37,14 @@ echo \
 sudo apt-get update
 sudo apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin
 sudo docker run hello-world
+
+# Docker auth
+sudo groupadd docker
+sudo usermod -aG docker $USER
+newgrp docker
+docker run hello-world
+
+# Docker Compose
+sudo curl -L "https://github.com/docker/compose/releases/download/2.13.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+docker-compose --version
