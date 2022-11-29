8a11e8c65c98e4d824556cfe2d40cc452ef2fad4
asnared <abigger87@gmail.com>
2022-11-28 17:41:35 -0800

    fuse

--- scripts/installs.sh
+++ scripts/installs.sh
@@ -61,5 +61,6 @@ sudo apt -y install virtualbox
 wget https://releases.hashicorp.com/vagrant/2.3.3/vagrant_2.3.3-1_amd64.deb
 sudo apt -y install ./vagrant_2.3.3-1_amd64.deb
 
-
+# Fuse (for app images)
+sudo apt install libfuse2
 
