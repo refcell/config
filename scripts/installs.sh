26520a9542ab0947749dbba93543513b23726be8
asnared <abigger87@gmail.com>
2022-11-28 16:34:04 -0800

    :gear: tweaks

--- scripts/installs.sh
+++ scripts/installs.sh
@@ -48,3 +48,8 @@ docker run hello-world
 sudo curl -L "https://github.com/docker/compose/releases/download/2.13.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 docker-compose --version
+
+# Ubuntu tweaks
+sudo add-apt-repository universe
+sudo apt install gnome-tweaks
+sudo apt install gnome-shell-extension-gsconnect
